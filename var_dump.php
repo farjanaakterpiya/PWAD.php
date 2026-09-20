80adb928476f8dc91de0278807a0e0a309f1b2f0
farjanaakterpiya <farjanaakterpiya29@gmail.com>
2026-09-19 21:23:17 -0700

    Add files via upload

---
+++ var_dump.php
@@ -0,0 +1,9 @@
+<?php
+$flowers = ["Rose","Waterlily","Sunflower","Tultip","Marigold"];
+$number = 100;
+$name = "Tanni";
+echo "<pre>";
+var_dump($flowers);
+var_dump($number);
+var_dump($name);
+?>
---
+++ variable.php
@@ -0,0 +1,11 @@
+<?php
+$x =15;
+$y = "Hello word";
+// echo $x;
+print $x;
+?>
+
+<?php
+$color = "red";
+echo "<br>My car is $color<br>";
+?>
